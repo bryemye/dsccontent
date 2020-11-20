944883866bb06688df18c2c2641bd0392269ab53
briancmiles <briancmiles@penguin>
2020-11-20 02:01:56 -0600

    make it better

--- portal/settings.py
+++ portal/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'portal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portal',
+        'USER': 'briancmiles',
+        'PASSWORD' : 'Majikh0le!@',
+        'HOST' : '',
+        'PORT': '5432',
     }
 }
 
